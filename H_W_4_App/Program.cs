78d6dabe446598d59c6aef7a1a07b20c500b5e60
Nastkav <faghyt5@gmail.com>
2022-12-18 01:27:26 +0300

    StyleCop changes

--- H_W_4_App/Program.cs
+++ H_W_4_App/Program.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Reflection;
-
-namespace H_W_4_App
+﻿namespace H_W_4_App
 {
+    using System;
+    using System.Reflection;
 
+    /// <summary>
+    /// Start point.
+    /// </summary>
     class Program
     {
-
-        static void Main(string[] args)
+        /// <summary>
+        /// Start of app.
+        /// </summary>
+        /// <param name="args">args added ...</param>
+        public static void Main(string[] args)
         {
             Console.Write("Size ");
             int size = Convert.ToInt32(Console.ReadLine());
@@ -17,8 +22,6 @@ namespace H_W_4_App
             List<char> list = new List<char>();
             int counterEv = 0;
             int counterOdd = 0;
-
-
             for (char c = 'a'; c <= 'z'; ++c)
             {
                 list.Add(c);
@@ -30,7 +33,7 @@ namespace H_W_4_App
             {
                 nums[i] = new Random().Next(1, 26);
                 Console.Write($"{nums[i]} ");
-                if(nums[i] % 2 == 0)
+                if (nums[i] % 2 == 0)
                 {
                     even.Add(alphabet[nums[i]]);
                 }
@@ -39,47 +42,54 @@ namespace H_W_4_App
                     odd.Add(alphabet[nums[i]]);
                 }
             }
+
             Console.WriteLine();
 
             foreach (char c in even)
             {
-                if (c == 'a' || c == 'e' || c == 'i' )
+                if (c == 'a' || c == 'e' || c == 'i')
                 {
-                    Console.Write($"{Char.ToUpper(c)} ");
+                    Console.Write($"{char.ToUpper(c)} ");
                     counterEv++;
                 }
                 else
+                {
                     Console.Write($"{c} ");
+                }
             }
+
             Console.WriteLine();
             foreach (char c in odd)
             {
                 if (c == 'd' || c == 'h' || c == 'j')
                 {
-                    Console.Write($"{Char.ToUpper(c)} "); 
+                    Console.Write($"{char.ToUpper(c)} ");
                     counterOdd++;
                 }
                 else
+                {
                     Console.Write($"{c} ");
+                }
             }
+
             Console.WriteLine();
 
-            if(counterOdd < counterEv)
+            if (counterOdd < counterEv)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("There are more capital letters in an even list");
                 Console.ResetColor();
             }
-            else if(counterOdd > counterEv)
+            else if (counterOdd > counterEv)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("There are more capital letters in an odd list");
                 Console.ResetColor();
             }
             else
+            {
                 Console.WriteLine("Even and odd lists have the same number of capital letters");
-           
-                
+            }
         }
     }
 }
